c954528a493c64e8ff1c08fa119f2e5596d73839
Basit Zahid <basit.zahid@yahoo.com>
2024-05-30 16:31:43 -0500

    added new field and tested working successfully

--- coding_challenge/movies/serializers/movie_serializer.py
+++ coding_challenge/movies/serializers/movie_serializer.py
@@ -2,13 +2,20 @@ from rest_framework import serializers
 
 from movies.models import Movie
 
+class RuntimeFormattedField(serializers.Field):
+    def to_representation(self, value):
+        hours = value // 60
+        minutes = value % 60
+        return f"{hours}:{minutes:02d}"
 
 class MovieSerializer(serializers.ModelSerializer):
+    runtime_formatted = RuntimeFormattedField(source='runtime')
     class Meta:
         model = Movie
         fields = (
             "id",
             "title",
             "runtime",
+            "runtime_formatted",
             "release_date",
         )
