294ae8e087cb0df110cfa6d76b61c772805df2e5
Basit Zahid <basit.zahid@yahoo.com>
2024-05-30 16:40:23 -0500

    added review model with reviewer_name and rating field

--- coding_challenge/movies/models/movie.py
+++ coding_challenge/movies/models/movie.py
@@ -5,3 +5,8 @@ class Movie(models.Model):
     title = models.CharField(max_length=100, unique=True)
     runtime = models.PositiveIntegerField()
     release_date = models.DateField()
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
+    reviewer_name = models.CharField(max_length=100)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
